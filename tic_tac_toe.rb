1548b65e3e24dbc03679998730c750ae0d02ec8a
pjpzaf <paolojosemari.zafra@gmail.com>
2024-03-23 07:38:59 +0800

    added print board feature in GameBoard class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -14,7 +14,9 @@ class GameBoard
                     "b1", "b2", "b3",
                     "c1", "c2", "c3"
                   ]
-                  @player_move
+
+                  # @player_move = player_move
+
   end
 
   def set_board (player_move)
@@ -22,8 +24,20 @@ class GameBoard
       puts "Not enough input! Please provide 'grid_position, chosen symbol (x or o)' in the command line. i.e. 'a2, x'"
     elsif player_move.length > 2
       puts "Too many input! Please provide 'grid_position, chosen symbol (x or o)' in the command line. i.e. 'a2, x'"
-    elsif player_move.lenght == 2
-      
+    elsif player_move.length == 2
+
+      # print board in terminal
+      puts "The game board is now:"
+      loop_counter = 0
+      game_board.each do |position|
+        if (loop_counter == 2) || (loop_counter == 5) || (loop_counter == 8)
+           puts " #{position} "
+        else
+          print " #{position} "
+        end
+        loop_counter += 1
+      end
+
     end
   end
 
@@ -41,3 +55,4 @@ class GamePlayer
 end
 
 
+new_game = GameBoard.new
