0597c8500b120ef730bc77d3630b502001e47c46
pjpzaf <paolojosemari.zafra@gmail.com>
2024-05-27 07:51:17 +0800

    Added all functions and the application is now working at the limit of 10rounds only

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,7 @@
 require "pry"
 require "pry-byebug"
 
+# create a new game
 class NewGame
   attr_accessor :game_board
   def initialize ()
@@ -25,8 +26,8 @@ class NewGame
       end
   end
 
-
-  def eval_game (player)
+  # function to evaluate game after every round
+  def eval_game (player,round)
     if ((game_board[0]==player.choice && game_board[1]==player.choice && game_board[2]==player.choice)) ||
        ((game_board[3]==player.choice && game_board[4]==player.choice && game_board[5]==player.choice)) ||
        ((game_board[6]==player.choice && game_board[7]==player.choice && game_board[8]==player.choice)) ||
@@ -36,19 +37,26 @@ class NewGame
        ((game_board[0]==player.choice && game_board[4]==player.choice && game_board[8]==player.choice)) ||
        ((game_board[2]==player.choice && game_board[4]==player.choice && game_board[6]==player.choice)) 
 
+        puts ""
+        print_board()
+        puts ""
         puts "#{player.name} wins this round!"
         player.score += 1
         inner_loop_counter = 1
+    elsif (round==8)
+      inner_loop_counter = 1
+      puts "All positions are filled, nobody wins this round!"
     else
       inner_loop_counter = 0
     end
     return inner_loop_counter
   end
 
-
+  # create a new game round
   def game_rounds (player_one, player_two)
     for round in 0..8
       inner_loop_counter = 0
+      # player one's "X" turn
       if round.even?
         print_board()
         puts " "
@@ -62,7 +70,26 @@ class NewGame
             loop_counter = 1
             choice_index = game_board.find_index(choice_position)
             game_board[choice_index] = player_one.choice
-            inner_loop_counter = eval_game(player_one)
+            inner_loop_counter = eval_game(player_one,round)
+          else
+            puts "Invalid input! Please select a position in the gameboard shown below"
+          end
+        end
+      # player two's "O" turn
+      elsif round.odd?
+        print_board()
+        puts " "
+        loop_counter = 0
+        until loop_counter == 1 do
+          puts "#{player_two.name} please choose a position in the board to put 'O'"
+          choice_position = gets
+          choice_position.downcase!
+          choice_position.gsub!(/\s+/, '')
+          if self.game_board.include?(choice_position)
+            loop_counter = 1
+            choice_index = game_board.find_index(choice_position)
+            game_board[choice_index] = player_two.choice
+            inner_loop_counter = eval_game(player_two,round)
           else
             puts "Invalid input! Please select a position in the gameboard shown below"
           end
@@ -75,7 +102,7 @@ class NewGame
   end
 end
 
-
+# creates a new player
 class GamePlayer < NewGame
   attr_accessor :score, :choice_position
   attr_reader :name, :choice 
@@ -83,13 +110,12 @@ class GamePlayer < NewGame
     @name = name
     @score = score
     @choice = choice
-    @choice_position = choice_positionosition = choice_position
+    @choice_position = choice_position
   end
 end
 
 
-#Start of game
-
+# Start of game
 puts " "
 puts "Let's play Tic Tac Toe!"
 puts " "
@@ -138,5 +164,18 @@ else
 end
 end
 
-test = NewGame.new
-test.game_rounds(player_one, player_two)
+for game_round in 0..(game_rounds - 1)
+
+this_round = NewGame.new
+this_round.game_rounds(player_one, player_two)
+
+if (game_round==(game_rounds-1))
+  if (player_one.score > player_two.score)
+    puts "#{player_one.name} wins the match! Congratulations!"
+  elsif (player_two.score > player_one.score)
+    puts "#{player_two.name} wins the match! Congratulations!"
+  elsif (player_one.score==player_two.score)
+    puts "It's a tie at #{player_one.score} each! Better luck next time ;)"
+  end
+end
+end
